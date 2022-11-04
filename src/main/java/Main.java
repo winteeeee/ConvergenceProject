4c3adb6e372f1926a8b29e93d5c587f82cbbf21e
82106 <erohas0521@naver.com>
2022-11-04 15:18:19 +0900

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("ㅎㅇ");
-        System.out.println("test");
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("시발");
+
     }
 }
