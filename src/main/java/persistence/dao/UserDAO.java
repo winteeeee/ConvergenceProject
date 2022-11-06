a2fb31fba894db3d2065f2ea9b282a905df886f0
82106 <erohas0521@naver.com>
2022-11-06 22:32:39 +0900

    UserDAO 임시 설계

--- src/main/java/persistence/dao/UserDAO.java
+++ src/main/java/persistence/dao/UserDAO.java
@@ -2,13 +2,16 @@ package persistence.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import persistence.dto.UserDTO;
+import persistence.enums.Authority;
+import java.util.List;
 
 public class UserDAO {
     private SqlSessionFactory sqlSessionFactory = null;
     public UserDAO(SqlSessionFactory sqlSessionFactory){
         this.sqlSessionFactory = sqlSessionFactory;
     }
-    public UserDTO findUserWithId() {
+
+    public UserDTO findUserWithId(String id) {
         UserDTO user = null;
         SqlSession session = sqlSessionFactory.openSession();
         try{
@@ -17,5 +20,21 @@ public class UserDAO {
             session.close();
         }
         return user;
+    }
+
+    public void insertUser(String id, String pw, Authority authority, String address) {
+
+    }
+
+    public void updateUser(UserDTO dto) {
+
+    }
+
+    public UserDTO readOneUser(String id) {
+        return null;
+    }
+
+    public List<UserDTO> readAllUser() {
+        return null;
     }
 }
