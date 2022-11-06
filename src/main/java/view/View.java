ba08bb4e8851502b8fb3a7e78ed74b5632b89f14
82106 <erohas0521@naver.com>
2022-11-06 22:47:13 +0900

    temp

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,9 +1,4 @@
 package view;
 
-public class View {/*
-    public void printAll(List<DTO> dtos) {
-        for(DTO dto : dtos) {
-            System.out.println("dto.toString() = " + dto.toString());
-        }
-    }*/
+public class View {
 }
