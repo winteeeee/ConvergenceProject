aa25fdb2b974c7033e1712c0e15705f5c98ee446
82106 <erohas0521@naver.com>
2022-11-06 21:22:44 +0900

    temp

--- src/main/java/persistence/dao/UserDAO.java
+++ src/main/java/persistence/dao/UserDAO.java
@@ -2,20 +2,31 @@ package persistence.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import persistence.dto.UserDTO;
+import persistence.enums.Authority;
 
 public class UserDAO {
     private SqlSessionFactory sqlSessionFactory = null;
     public UserDAO(SqlSessionFactory sqlSessionFactory){
         this.sqlSessionFactory = sqlSessionFactory;
     }
-    public UserDTO findUserWithId() {
+
+    public UserDTO findUserWithId(String id) {
         UserDTO user = null;
         SqlSession session = sqlSessionFactory.openSession();
         try{
-            user = session.selectOne("");
+            user = session.selectOne("", id);
         } finally {
             session.close();
         }
         return user;
+    }
+
+    public void insertUser(String id, String pw, Authority authority, String address) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            // 세션을 사용해서 DB에 유저 넣기
+        } finally {
+            session.close();
+        }
     }
 }
