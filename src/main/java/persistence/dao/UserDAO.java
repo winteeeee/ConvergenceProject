7f3a0e0baf306f95138403190c986106add8c116
82106 <erohas0521@naver.com>
2022-11-07 17:37:09 +0900

    UserDAO 예상 구현

--- src/main/java/persistence/dao/UserDAO.java
+++ src/main/java/persistence/dao/UserDAO.java
@@ -23,18 +23,44 @@ public class UserDAO {
     }
 
     public void insertUser(String id, String pw, Authority authority, String address) {
-
+        UserDTO user = new UserDTO(id, pw, 0, address);
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            session.insert("mapper.UserMapper.쿼리명", user);
+        } finally {
+            session.close();
+        }
     }
 
     public void updateUser(UserDTO dto) {
-
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            session.update("mapper.UserMapper.쿼리명", dto);
+        } finally {
+            session.close();
+        }
     }
 
     public UserDTO readOneUser(String id) {
-        return null;
+        UserDTO user = null;
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            user = session.selectOne("mapper.UserMapper.쿼리명", id);
+        } finally {
+            session.close();
+        }
+        return user;
     }
 
     public List<UserDTO> readAllUser() {
-        return null;
+        List<UserDTO> list = null;
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            list = session.selectList("mapper.UserMapper.쿼리명");
+        }
+        finally {
+            session.close();
+        }
+        return list;
     }
 }
