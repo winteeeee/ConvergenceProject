ce3e7a12623129e7d75d4cb6d95c9063738eaa6e
DoDoGaMaRu <nm09090@gmail.com>
2022-11-09 18:21:11 +0900

    temp

--- src/main/java/persistence/dao/UserDAO.java
+++ src/main/java/persistence/dao/UserDAO.java
@@ -2,20 +2,67 @@ package persistence.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import persistence.dto.UserDTO;
+import persistence.enums.Authority;
+import java.util.List;
 
 public class UserDAO {
     private SqlSessionFactory sqlSessionFactory = null;
     public UserDAO(SqlSessionFactory sqlSessionFactory){
         this.sqlSessionFactory = sqlSessionFactory;
     }
-    public UserDTO findUserWithId() {
+
+    public boolean findUser(String id) {
+        boolean isUser = false;
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            if(session.selectOne("mapper.UserMapper.쿼리명", id) != null) {
+                isUser = true;
+            }
+        } finally {
+            session.close();
+        }
+        return isUser;
+    }
+
+    public void insertUser(String id, String pw, Authority authority, String address) {
+        UserDTO user = new UserDTO(id, pw, Authority.USER.getCode(), address);
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            session.insert("mapper.UserMapper.쿼리명", user);
+        } finally {
+            session.close();
+        }
+    }
+
+    public void updateUser(UserDTO user) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            session.update("mapper.UserMapper.쿼리명", user);
+        } finally {
+            session.close();
+        }
+    }
+
+    public UserDTO readOneUser(String id) {
         UserDTO user = null;
         SqlSession session = sqlSessionFactory.openSession();
         try{
-            user = session.selectOne("");
+            user = session.selectOne("mapper.UserMapper.쿼리명", id);
         } finally {
             session.close();
         }
         return user;
+    }
+
+    public List<UserDTO> readAllUser() {
+        List<UserDTO> list = null;
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            list = session.selectList("mapper.UserMapper.쿼리명");
+        }
+        finally {
+            session.close();
+        }
+        return list;
     }
 }
