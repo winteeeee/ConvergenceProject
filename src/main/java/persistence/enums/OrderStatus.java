0c368e4e5ca4184bcec7f521683e1d5a93855927
DoDoGaMaRu <nm09090@gmail.com>
2022-11-11 21:08:30 +0900

    enum 추가 및 적용

--- src/main/java/persistence/enums/OrderStatus.java
+++ src/main/java/persistence/enums/OrderStatus.java
@@ -1,29 +1,31 @@
 package persistence.enums;
 
 import lombok.Getter;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Getter
-public enum Status implements Enum {
-    ACCEPT(0, "수락"),
-    REJECT(1, "거절"),
-    HOLD(2, "보류");
+public enum OrderStatus implements Enum {
+    HOLD(0, "접수대기"),
+    CANCEL(1, "취소"),
+    IN_DELIVERY(2, "배달중"),
+    COMPLETE(3, "배달완료");
 
     private static final Map<Integer, String> CODE_MAP = Collections.unmodifiableMap(
-            Stream.of(values()).collect(Collectors.toMap(Status::getCode, Status::name))
+            Stream.of(values()).collect(Collectors.toMap(OrderStatus::getCode, OrderStatus::name))
     );
 
-    public static Status of(int code) {
-        return Status.valueOf(CODE_MAP.get(code));
+    public static OrderStatus of(int code) {
+        return OrderStatus.valueOf(CODE_MAP.get(code));
     }
 
     private final int code;
     private final String title;
 
-    Status(int code, String title) {
+    OrderStatus(int code, String title) {
         this.code = code;
         this.title = title;
     }
