a514df70deccb7ac70b8b2059dd275b47a411668
DoDoGaMaRu <nm09090@gmail.com>
2022-12-03 23:43:52 +0900

    [persistence] 서비스 완성(추가)

---
+++ src/main/java/persistence/dto/StatisticsDTO.java
@@ -0,0 +1,29 @@
+package persistence.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import persistence.enums.OrdersStatus;
+import persistence.enums.RegistStatus;
+
+@Getter
+@Setter
+@Builder
+public class StatisticsDTO extends DTO {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer count;
+
+    public StatisticsDTO() {}
+
+    public StatisticsDTO(Long id, String name, Integer price, Integer count) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+    public StatisticsDTO(byte[] arr) {
+        setMembersByByteArray(arr);
+    }
+}
