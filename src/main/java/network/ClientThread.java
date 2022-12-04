d14fc96461faa5fc9a33b8d3bb7a87fb9a996943
lks9902 <lks9902@naver.com>
2022-12-04 18:11:30 +0900

    ClientThread 내부 함수 대부분 구현 리뷰조회, 주문, 메뉴등록... 미완성

--- src/main/java/network/ClientThread.java
+++ src/main/java/network/ClientThread.java
@@ -1,6 +1,7 @@
 package network;
 
 import persistence.dto.*;
+import persistence.enums.Authority;
 import service.AdminService;
 import service.OwnerService;
 import service.UserService;
@@ -8,6 +9,8 @@ import service.UserService;
 import java.io.*;
 import java.net.ProtocolFamily;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.List;
 
 class ClientThread extends Thread {
     Socket socket;
@@ -21,8 +24,10 @@ class ClientThread extends Thread {
     private UserService userService;
     private OwnerService ownerService;
     private AdminService adminService;
-    int id;
-    UserDTO user;
+    private int id;
+    private UserDTO user;
+    private String authority;
+    private long store_id;
 
     ClientThread (Socket socket, int id) {
         this.socket = socket;
@@ -32,14 +37,15 @@ class ClientThread extends Thread {
     @Override
     public void run () {
         try {
-            while (true) {
-                dis = new DataInputStream(socket.getInputStream());
-                br = new BufferedReader(new InputStreamReader(dis));
+            dis = new DataInputStream(socket.getInputStream());
+            br = new BufferedReader(new InputStreamReader(dis));
 
-                dos = new DataOutputStream(socket.getOutputStream());
-                bw = new BufferedWriter(new OutputStreamWriter(dos));
+            dos = new DataOutputStream(socket.getOutputStream());
+            bw = new BufferedWriter(new OutputStreamWriter(dos));
 
-                readBuf = br.readLine().getBytes();
+            while (dis.read(readBuf) != -1) {
+                //readBuf = dis.readAllBytes();
+                //readBuf = br.readLine().getBytes();
                 Protocol protocol = new Protocol(readBuf);
 
                 selectFunction(protocol);
@@ -97,10 +103,13 @@ class ClientThread extends Thread {
                 store_search();
             }
             else if (code == (ProtocolCode.STORE | ProtocolCode.HISTORY)) {
-                store_history_search();
-            }
-            else if (code == ProtocolCode.ORDER) {
-                order_search();
+                if(authority.equals(Authority.OWNER)) {
+                    store_history_search((StoreDTO)data);
+                }
+                else {
+                    store_history_search();
+                }
+
             }
             else if (code == (ProtocolCode.ORDER | ProtocolCode.HISTORY)) {
                 order_history_search();
@@ -121,25 +130,78 @@ class ClientThread extends Thread {
 
     }
 
-    private void order_history_search() {
-
+    private void order_history_search() throws IOException {
+        List<TotalOrdersDTO>totalOrdersDTOs = userService.getOrders(user.getPk());
+        for(int i = 0; i < totalOrdersDTOs.size(); i++) {
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, totalOrdersDTOs.size(), totalOrdersDTOs.get(i));
+            dos.write(send_protocol.getBytes());
+        }
     }
 
-    private void order_search() {
-
+    private void store_history_search(StoreDTO storeDTO) throws IOException {
+        List<StatisticsDTO> statisticsDTOs = ownerService.getStatistics(storeDTO.getId());
+        for(int i = 0; i < statisticsDTOs.size(); i++) {
+            StatisticsDTO statisticsDTO = statisticsDTOs.get(i);
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, statisticsDTOs.size(), statisticsDTO);
+            dos.write(send_protocol.getBytes());
+        }
     }
 
-    private void store_history_search() {
-
+    private void store_history_search() throws IOException {
+        List<StatisticsDTO> statisticsDTOs = adminService.getStatistics();
+        for(int i = 0; i < statisticsDTOs.size(); i++) {
+            StatisticsDTO statisticsDTO = statisticsDTOs.get(i);
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, statisticsDTOs.size(), statisticsDTO);
+            dos.write(send_protocol.getBytes());
+        }
     }
 
-    private void store_search() {
-
+    private void store_search() throws IOException {
+        List<StoreDTO>stores = userService.getAllStore();
+        StoreDTO store;
+        for(int i = 0; i < stores.size(); i++) {
+            store = stores.get(i);
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, stores.size(), store);
+            dos.write(send_protocol.getBytes());
+        }
     }
 
     private void user_search(UserDTO userDTO) throws IOException {
         UserDTO temp = userService.getUserWithId(userDTO.getId());
         if (temp.getPw().equals(userDTO.getPw())) {
+            user = temp;
+            authority = user.getAuthority();
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, user);
+        }
+        else {
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.REFUSAL, 0, null);
+        }
+        dos.write(send_protocol.getBytes());
+    }
+
+    private void order_modify(OrdersDTO orderDTO) throws IOException {
+        if (userService.cancelOrder(orderDTO.getTotal_orders_id()) == 1) {
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, null);
+        }
+        else {
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.REFUSAL, 0, null);
+        }
+        dos.write(send_protocol.getBytes());
+    }
+
+    private void store_modify(StoreDTO storeDTO) throws IOException {
+        if (ownerService.updateTime(storeDTO.getId(), storeDTO.getOpen_time(), storeDTO.getClose_time()) == 1) {
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, null);
+        }
+        else {
+            send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.REFUSAL, 0, null);
+        }
+        dos.write(send_protocol.getBytes());
+    }
+
+    private void user_modify(UserDTO userDTO) throws IOException {
+        UserDTO temp = userService.update(userDTO);
+        if (temp != null) {
             user = temp;
             send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, user);
         }
@@ -149,28 +211,38 @@ class ClientThread extends Thread {
         dos.write(send_protocol.getBytes());
     }
 
-    private void order_modify(OrdersDTO orderDTO) {
-
+    private void order_register(OrdersDTO orderDTO) throws IOException {
+        send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.REFUSAL, 0, null);
+        if (openTime < LocalDateTime.now() && LocalDateTime.now() < closeTime) {
+            if (userService. == 1) {
+                send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, null);
+            }
+        }
+        dos.write(send_protocol.getBytes());
     }
 
-    private void store_modify(StoreDTO storeDTO) {
-
-    }
-
-    private void user_modify(UserDTO userDTO) {
-
-    }
-
-    private void order_register(OrdersDTO orderDTO) {
-
-    }
-
-    private void review_register(ReviewDTO reviewDTO) {
-
+    private void review_register(ReviewDTO reviewDTO) throws IOException {
+        if (authority.equals(Authority.OWNER)) {
+            if (ownerService.updateOwnerComment(reviewDTO.getId(), reviewDTO.getComment()) == 1) {
+                send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, null);
+            }
+            else {
+                send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.REFUSAL, 0, null);
+            }
+        }
+        else if (authority.equals(Authority.USER)) {
+            if (userService.writeReview(reviewDTO) == 1) {
+                send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.ACCEPT, 0, null);
+            }
+            else {
+                send_protocol = new Protocol(ProtocolType.RESPONSE, ProtocolCode.REFUSAL, 0, null);
+            }
+        }
+        dos.write(send_protocol.getBytes());
     }
 
     private void menu_register(MenuDTO menuDTO) {
-
+        //ownerService.insertMenu(menuDTO);
     }
 
     private void store_register(StoreDTO storeDTO) throws IOException {
